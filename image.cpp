2f2c8ec72f2c66934414ad10710362e5bde3f6e9
f19003179 <arsene.fougerouse@etu.univ-amu.fr>
2020-06-01 19:21:56 +0200

    Création de la classe image

---
+++ image.cpp
@@ -0,0 +1,32 @@
+//Image.cpp
+
+#include <iostream>
+
+using namespace std;
+
+#include "Image.hpp"
+
+Image::Image(ulong _couleur, uint _x, uint _y, uint _largeur, uint _longueur, const char *_nomFichier)
+ : Forme(_couleur,_x,_y), largeur(_largeur), longueur(_longueur), nomFichier(_nomFichier)
+{}
+
+Image::Image(const Image &orig)
+ : Forme(orig),largeur(orig.largeur),longueur(orig.longueur),nomFichier(orig.nomFichier)
+{}
+
+Image::Image(istream &is)
+ : Forme(is),largeur(0),longueur(0),nomFichier(nullptr)
+{
+    is >> *nomFichier >> largeur >> longueur;
+}
+
+Image::~Image()
+{
+    delete nomFichier;
+}
+
+void Image::dessiner(EZWindow &w, bool isActive) const
+{
+    Forme::dessiner(w, isActive);
+    nomFichier::EZImage(longueur, largeur);
+}
---
+++ image.hpp
@@ -0,0 +1,32 @@
+//Image.hpp
+
+#ifndef IMAGE_HPP
+#define IMAGE_HPP
+
+#include "Forme.hpp"
+
+class Image : public Forme {
+    private :
+        char * nomFichier;
+        uint longueur;
+        uint largeur;
+    
+    public :
+        Image(ulong _couleur, uint _x, uint _y, uint _largeur, uint _longueur, const char *_nomFichier);
+        Image(const Image& orig);
+        Image(istream& is);
+        ~Image();
+
+        inline uint getLargeur() const { return largeur; }
+        inline uint getlongueur() const { return longueur; }
+
+        inline void setLargeur(uint _largeur) { largeur=_largeur; }
+        inline void setlongueur(uint _longueur) { longueur = _longueur; }
+        void dessiner(EZWindow &w, bool isActive) const override;
+        double perimetre() const;
+
+        protected:
+        virtual ostream& ecrire(ostream& os) const; 
+};
+
+#endif
